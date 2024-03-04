d6c16dfa384f8589183f9babca7f6727bcdb571d
lakshman7781 <lakshmanbhavanam009@gmail.com>
2024-03-05 00:30:08 +0530

    added code

---
+++ src/data/constants.js
@@ -0,0 +1,216 @@
+import { Javascript } from "@mui/icons-material";
+
+export const Bio = {
+  name: "lakshman reddy",
+  roles: [
+    "Full Stack Developer",
+    "tech enthusiast",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/lakshman7781",
+  resume:
+    "https://drive.google.com/file/d/1U6kpJaz67xr9YRMTm_8LDJevN9Ri8jtG/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/lakshman-bhavanam-6b7044259/",
+  twitter: "https://twitter.com/lakshmanbhavan7",
+  insta: "https://www.instagram.com/lakshman_bhavanam/",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      
+      
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      
+      
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+      
+      
+    ],
+  },
+  
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img:"https://firebasestorage.googleapis.com/v0/b/portfolio-8ccd0.appspot.com/o/srkr.jpg?alt=media&token=e5f84b97-12d5-4d04-ad1d-d85e0dc3c4fc",
+    role: "junior software developer,SRKREC",
+    desc:"I am currently working as a junior software developer at SRKR Engineering College, Bhimavaram. I am responsible for developing and maintaining the college's website and other software applications.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "php",
+    ],
+  },
+  // {
+  //   id: 1,
+  //   img:"https://firebasestorage.googleapis.com/v0/b/portfolio-8ccd0.appspot.com/o/srkr.jpg?alt=media&token=e5f84b97-12d5-4d04-ad1d-d85e0dc3c4fc",
+  //   role: "junior software developer,SRKREC",
+  //   desc:"I am currently working as a junior software developer at SRKR Engineering College, Bhimavaram. I am responsible for developing and maintaining the college's website and other software applications.",
+  //   skills: [
+  //     "HTML",
+  //     "CSS",
+  //     "JavaScript",
+  //     "php",
+  //   ],
+  //   doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  // },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img:"https://firebasestorage.googleapis.com/v0/b/portfolio-8ccd0.appspot.com/o/srkr.jpg?alt=media&token=e5f84b97-12d5-4d04-ad1d-d85e0dc3c4fc",
+    school: "srkr engineering college, Bhimavarm",
+    date: "2022 - 2026",
+    grade: "8.9 CGPA [current]",
+    desc: "I am currently pursuing 2nd year btech  in Computer Science and Engineering at srkr engineering college, Bhimavaram. ",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering ",
+  },
+  {
+    id: 1,
+    img:"https://firebasestorage.googleapis.com/v0/b/portfolio-8ccd0.appspot.com/o/narayana.clg.png?alt=media&token=69267cb2-79d4-4f94-bf8c-2412b574eebf",
+    school: "narayana junior college, vijawada",
+    date: "2020-2022",
+    grade: "94.44%",
+    desc: "I completed my intermediate  education at narayana junior college, vijayawada.",
+    degree: "intermediate education",
+  },
+  {
+    id: 2,
+    img:"https://firebasestorage.googleapis.com/v0/b/portfolio-8ccd0.appspot.com/o/universal.png?alt=media&token=715039ac-db4d-4c72-bbeb-0a84392e6ae0",
+    school: "universal high school, puaspadu",
+    date: "2019-2020",
+    grade: "94.84%",
+    desc: "I completed my class 10 education at universal high School, puaspadu.",
+    degree: "class 10",
+  },
+];
+
+export const projects = [
+  
+  
+  
+  
+  
+  {
+    id: 0,
+    title: "Campus Online",
+    date: "feb 2024",
+    description:
+      "capmus online is a platfoem for campus commerce and communication. It is a web application that allows students to buy and sell products within the campus. It also provides a platform for students to communicate with each other.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8ccd0.appspot.com/o/Campus.png?alt=media&token=ccea6c73-2a57-4e10-925f-04f06d4bc0ac",
+    tags: ["php","Javascript","HTML","CSS"],
+    category: "web app",
+    github: "https://github.com/lakshman7781/hackathon",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
